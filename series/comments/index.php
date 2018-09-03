251a167ea66c876784ea20c77e359c050b638e60
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-02 23:41:58 -0400

    Add files via upload

--- series/comments/index.php
+++ series/comments/index.php
@@ -1,6 +1,6 @@
 <?php
-/* Author: Brian Savage
-Date: 08/25/2018 */
+/* Author: Nelson Dominguez
+Date: 09/2/2018 */
 // Uses echo language construct to output the text "Hello World." to the browser.
 echo 'Hello World.';
 ?>
