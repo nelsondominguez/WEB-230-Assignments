b18135bba8cc8b4b8ee70cb3f5aacf5ff656c665
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-09 06:13:48 -0400

    Add files via upload

---
+++ series/concatenation/index.php
@@ -0,0 +1,12 @@
+<?php
+
+     $day = 'Monady';
+     $date = 30;
+     $year = 2018;
+     $month = 'July';
+
+      echo "The date is $day $date $year $month";
+      echo '<br/>';
+      echo 'The date is '.$day.' '.$date.' '.$year.' '.$month;
+
+?>
