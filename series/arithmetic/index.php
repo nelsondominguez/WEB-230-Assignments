19453bf2cbb21bad554eb664115848462744935b
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-16 19:45:59 -0400

    Add files via upload

---
+++ series/arithmetic/index.php
@@ -0,0 +1,63 @@
+<?php
+
+$sum = 10 + 20;
+
+echo $sum;
+
+echo '<br />';
+
+$sum = 10 - 20;
+
+echo $sum;
+
+echo '<br />';
+
+$sum = 10 * 20;
+
+echo $sum;
+
+echo '<br />';
+
+$sum = 10 / 20;
+
+echo $sum;
+
+echo '<br />';
+
+$number1 = 100;
+
+$number2 = 50;
+
+$number3 = 2;
+
+$result = $number1 + $number2 / $number3;
+
+echo $result;
+
+echo '<br />';
+
+$number1 = 10;
+
+$number2 = 3;
+
+$result = $number1 % $number2;
+
+echo $result;
+
+echo '<br />';
+
+$number1 = 10;
+
+$number1++;
+
+echo $number1;
+
+echo '<br />';
+
+$number1--;
+
+echo $number1;
+
+echo '<br />';
+
+?>
