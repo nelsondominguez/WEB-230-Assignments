13610c001ee7bf46cf3e5a9140d8e606b9ca0b98
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-23 22:23:40 -0400

    Add files via upload

---
+++ series/functions-and-global-vars/index.php
@@ -0,0 +1,17 @@
+<?php     
+
+  $user_ip = $_SERVER['REMOTE_ADDR'];
+
+  function echo_ip(){
+
+    global $user_ip;
+
+    $string = 'Your IP address is '.$user_ip;
+
+    echo $string;
+
+  }
+
+  echo_ip();
+  
+?>
