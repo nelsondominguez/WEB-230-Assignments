6eb49dba35842e2968594bc892e26691af0c2917
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-30 19:59:50 -0400

    Add files via upload

---
+++ series/include-and-require/index.php
@@ -0,0 +1,13 @@
+<?php          
+
+  include 'header.inc.php';
+
+  //require 'header.inc.php';
+
+  //include 'header2.inc.php';
+
+  //require 'header2.inc.php';
+
+  echo 'Var2 is '.$var2;
+
+?>
