47f803e4ec88eba1d74fe19ca73f70c549eb6da0
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-09-30 20:34:21 -0400

    Add files via upload

---
+++ series/include_once-and-require_once/header.inc.php
@@ -0,0 +1,9 @@
+<h1>Alex's page</h1>
+
+<?php
+
+  $var1 = 'String';
+
+  $var2 = 10;
+  
+?>
---
+++ series/include_once-and-require_once/index.php
@@ -0,0 +1,11 @@
+ <?php
+
+  //include 'header.inc.php';
+
+  include_once 'header.inc.php';
+
+  require 'header.inc.php';
+
+  echo 'Var2 is '.$var2;
+
+?>
