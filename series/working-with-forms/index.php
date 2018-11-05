c708303ae9347cdbcefdecf2a92ca6c9dea68c19
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-11-04 20:33:49 -0500

    Add files via upload

---
+++ series/working-with-forms/index.php
@@ -0,0 +1,18 @@
+<!--
+
+  01. Where the data is being sent.
+
+  02. How the data is being sent.
+
+-->
+
+<form action="index.php" method="GET">
+
+  Day:<br/><input type="text" name="day"><br>
+
+  Date:<br/><input type="text" name="date"><br>
+
+  Year:<br/><input type="text" name="year"><br>
+
+</form>
+
