d90ad49203308d2333cd79a6fa3d1b3031451512
nelsondominguez <36046530+nelsondominguez@users.noreply.github.com>
2018-12-03 06:03:12 -0500

    Add files via upload

---
+++ series/mysql-select/connect.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+  $conn_error = 'Could not connect.';
+
+  $mysql_host = 'localhost';
+
+  $mysql_user = 'root';
+
+  $mysql_pass = '';
+
+  $mysql_db = 'a_database';
+
+  if(!$con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db)){
+
+    die($conn_error);
+
+  }else{
+    
+    //echo 'Connected!';
+  }
+?>
